b49518a30526a0cae13b7936072dcb3332dd3610
Armin Felder <armin.felder@osalliance.com>
2018-06-13 00:48:05 +0200

    make code useable

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# gridfsmigrate
-WIP!
+## RocketChat GridFS to Filesystem migration script
+
+migrate [command] [targetPath] [dbname]
+
+e.g. ./migrate dump /home/armin/rctemp/files meteor
+
+### commands
+- dump :        dumps the GridFs stored files into the given folder and writers a log(log.csv)
+- updatedb :    changes the database entries to point to your stored files instead of GridFS
+- removeblobs : removes migrated files from GridFS
+
+### steps
+
+1. for safety do a mongo backup with mongodump
+2. switch RocketChat to FileSystem and set a folder to store the uploads(e.g. /app/uploads)
+3. run ./migrate dump [e.g /app/uploads] [e.g. rocketchat]
+4. run ./migrate updatedb [e.g /app/uploads] [e.g. rocketchat]
+5. have a look, if everything looks fine e.g are files missing etc.
+6. run ./migrate removeblobs [e.g /app/uploads] [e.g. rocketchat]
+
--- migrate.py
+++ migrate.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from pymongo import MongoClient
 import gridfs
 from pprint import pprint
@@ -7,30 +8,32 @@ import getopt
 import csv
 
 class Migrator():
-    def __init__(self, directory):
-        print("test")
+    def __init__(self, directory, db="rocketchat"):
         self.outDir = directory
         self.log = list()
+        self.db = db
 
     def dumpfiles(self, collection):
-        mime = MimeTypes()
-        db = MongoClient().rocketchat
-        fs = gridfs.GridFSBucket(db, collection)
-        for grid_out in fs.find({}, no_cursor_timeout=True):
-            data = grid_out.read()
-            pprint(grid_out.filename)
-            pprint(grid_out._id)
-            fileext = mime.guess_extension(grid_out.content_type)
-            filename = grid_out.filename
-            filename = '.'.join(filename.split('.')[:1])
-            if fileext:
-                filename = filename+fileext
 
-            print(filename)
-            file = open(self.outDir+"/"+filename, "wb")
-            file.write(data)
-            file.close()
-            self.addtolog(grid_out._id, filename, collection, grid_out.md5)
+        db = MongoClient()[self.db]
+        uploadsCol = db[collection]
+        fs = gridfs.GridFSBucket(db, collection)
+
+        uploads = uploadsCol.find({})
+
+        for upload in uploads:
+            if upload["store"] == "GridFS:Uploads":
+                path = upload["path"]
+                pathSegments = path.split("/")
+                gridfsId = pathSegments[3]
+                for res in fs.find({"_id": gridfsId}):
+                    data = res.read()
+                    fileext = upload["extension"]
+                    filename = gridfsId+"."+fileext
+                    file = open(self.outDir+"/"+filename, "wb")
+                    file.write(data)
+                    file.close()
+                    self.addtolog(gridfsId, filename, collection, res.md5)
         self.writelog()
 
     def addtolog(self, dbId, filename, collection, md5):
@@ -53,35 +56,68 @@ class Migrator():
 
     def updateDb(self):
         with open(self.outDir+"/log.csv") as csvfile:
-            db = MongoClient().rocketchat
+            db = MongoClient()[self.db]
             reader = csv.reader(csvfile, delimiter=',')
             for row in reader:
                 dbId = row[0]
                 filename = row[1]
-                collection = row[2]
+                collectionName = row[2]
                 md5 = row[3]
-                collection = db[collection]
+                collection = db[collectionName]
                 collection.update_one({
-                    "id": dbId
+                    "_id": dbId
                 }, {
                     "$set": {
-                        "store": "FileSystems:Uploads",
-                        "path": "/ufs/Filesystem:Uploads/"+dbId+"/"+filename,
-                        "url": "/ufs/Filesystem:Uploads/"+dbId+"/"+filename
+                        "store": "FileSystem:Uploads",
+                        "path": "/ufs/FileSystem:Uploads/"+dbId+"/"+filename,
+                        "url": "/ufs/FileSystem:Uploads/"+dbId+"/"+filename
                     }
                 })
+
+    def removeBlobs(self):
+        with open(self.outDir + "/log.csv") as csvfile:
+            db = MongoClient()[self.db]
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                dbId = row[0]
+                filename = row[1]
+                collectionName = row[2]
+                md5 = row[3]
+                collection = db[collectionName]
+                fs = gridfs.GridFSBucket(db, collectionName)
+                try:
+                    fs.delete(dbId)
+                except:
+                    continue
+
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        if len(sys.argv) == 3 and sys.argv[1] == "dump":
-            obj = Migrator(sys.argv[2])
+        if len(sys.argv) > 2 and sys.argv[1] == "dump":
+            obj = None
+            if len(sys.argv) == 4:
+                obj = Migrator(sys.argv[2],sys.argv[3])
+            else:
+                obj = Migrator(sys.argv[2])
             obj.dumpfiles("rocketchat_uploads")
             #obj.dumpfiles("custom_emoji")
             #obj.dumpfiles("assets")
-        elif len(sys.argv) == 3 and sys.argv[1] == "dedup":
-            obj = Migrator(sys.argv[2])
+        elif len(sys.argv) >2 and sys.argv[1] == "dedup":
+            if len(sys.argv) == 4:
+                obj = Migrator(sys.argv[2], sys.argv[3])
+            else:
+                obj = Migrator(sys.argv[2])
             obj.dedup()
-        elif len(sys.argv) == 3 and sys.argv[1] == "updatedb":
-            obj = Migrator(sys.argv[2])
+        elif len(sys.argv) >2 and sys.argv[1] == "updatedb":
+            if len(sys.argv) == 4:
+                obj = Migrator(sys.argv[2], sys.argv[3])
+            else:
+                obj = Migrator(sys.argv[2])
             obj.updateDb()
+        elif len(sys.argv) >2 and sys.argv[1] == "removeblobs":
+            if len(sys.argv) == 4:
+                obj = Migrator(sys.argv[2], sys.argv[3])
+            else:
+                obj = Migrator(sys.argv[2])
+            obj.removeBlobs()

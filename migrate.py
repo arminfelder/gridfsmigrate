df91e742d0bbc9fd0050f98d5b1a14b7c92a6381
arminfelder <armin.felder@gmail.com>
2019-04-05 14:12:15 +0200

    Update migrate.py

--- migrate.py
+++ migrate.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
+""" 
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Armin Felder"
+__contact__ = "armin.felder@osalliance.com"
+__copyright__ = "Copyright 2019, Armin Felder"
+__credits__ = ["all contributers"]
+__date__ = "2019.05.05"
+__deprecated__ = False
+__email__ =  "armin.felder@osalliance.com"
+__license__ = "GPLv3"
+__maintainer__ = "Armin Felder"
+__status__ = "Production"
+__version__ = "1.0.0"
+
 from pymongo import MongoClient
 import gridfs
 from pprint import pprint
